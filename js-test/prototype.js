c255ff884ab39bdcd100a3cff9c79c14856453d9
soyeon <lsyeon0825@gmail.com>
2023-12-14 11:13:24 +0900

    예제 수정

--- js-test/prototype.js
+++ js-test/prototype.js
@@ -9,8 +9,13 @@ User.prototype.getFullName = function () {
 }
 
 const soyeon = new User('soyeon', 'lee');
+const amy = new User('Amy', 'Clarke');
+const neo = new User('Neo', 'Smith');
 
-console.log(soyeon.getFullName());
+
+console.log(soyeon);
+console.log(amy.getFullName());
+console.log(neo.getFullName());
 
 // new 키워드를 통해 user라는 함수를 실행함 => 이를 생성자 함수라고 한다.
 // 하나의 객체데이터가 생성된다.
@@ -19,3 +24,5 @@ console.log(soyeon.getFullName());
 
 // 생성자함수는 일반함수와 구분이 안되기 때문에 new라는 키워드와 같이 사용되는 함수는
 // 파스칼 케이스(대문자로 시작해서 작성)로 해서 생성자함수로 구분할 수 있게 한다.(암묵적 개념)
+
+
